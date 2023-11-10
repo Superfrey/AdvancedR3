5c527644dcbad472b83e033cf7b8b7155de6fc69
Jonas Schaarup <jonasfrschaarup@gmail.com>
2023-11-10 08:07:26 +0100

    run loop analysis and extract estimates from predictors

--- R/functions.R
+++ R/functions.R
@@ -106,6 +106,38 @@ create_model_workflow <- function(model_specs, recipe_specs) {
 tidy_model_output <- function(workflow_fitted_model) {
     workflow_fitted_model %>%
         workflows::extract_fit_parsnip() %>%
-        broom::tidy(exponentiate = TRUE, conf.int = TRUE, conf.level = 0.95,
+        broom::tidy(exponentiate = TRUE
                     )
+}
+
+
+#' Generate the results of a model
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A data frame.
+#'
+generate_model_results <- function(data) {
+    create_model_workflow(
+        parsnip::logistic_reg() %>%
+            parsnip::set_engine("glm"),
+        data %>%
+            create_recipe_spec(tidyselect::starts_with("metabolite_"))
+    ) %>%
+        parsnip::fit(data) %>%
+        tidy_model_output()
+}
+
+
+#' Convert the long form dataset into a list of wide form data frames.
+#'
+#' @param data The lipidomics dataset.
+#'
+#' @return A list of data frames.
+#'
+split_by_metabolite <- function(data) {
+    data %>%
+        column_values_to_snake_case(metabolite) %>%
+        dplyr::group_split(metabolite) %>%
+        purrr::map(metabolites_to_wide)
 }
